e78286287ebc2c21c942304c620e97839b2746dd
Kacper <kacper.wozniak966@gmail.com>
2025-01-12 02:16:27 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   console.log({ JWT_SECRET: process.env.JWT_SECRET });
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [process.env.ORIGIN],
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: [process.env.ORIGIN],
+  //   credentials: true,
+  // });
+  app.enableCors();
   app.use(cookieParser());
   await app.listen(3000);
 }
